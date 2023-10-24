6114536c567ab5acd52ca70f7a7c26bc3d6b3c76
ttheshreeyasingh <shreeya.singh@students.iiit.ac.in>
2023-10-24 09:00:22 +0530

    implemented display prompt

---
+++ display.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <string.h>
+
+void displayPrompt() {
+    char *username = getpwuid(getuid())->pw_name;
+    char system_name[256];
+    gethostname(system_name, sizeof(system_name));
+
+    char current_dir[256];
+    getcwd(current_dir, sizeof(current_dir));
+
+    // Get user's home directory
+    char *home_dir = getpwuid(getuid())->pw_dir;
+
+    // Check if current_dir starts with home_dir
+    if (strstr(current_dir, home_dir) == current_dir) {
+        // Replace home_dir with ~
+        memmove(current_dir, "~", 1);
+        memmove(current_dir + 1, current_dir + strlen(home_dir), strlen(current_dir) - strlen(home_dir) + 1);
+    }
+
+    printf("<%s@%s:%s>\n", username, system_name, current_dir);
+}
